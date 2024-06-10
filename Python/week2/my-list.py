1ade3993b80b69bd0e21f9b1472a00103b768016
eniolajinadu <eniolajinadu792@gmail.com>
2024-06-10 08:22:43 +0100

    done

--- Python/week2/my-list.py
+++ Python/week2/my-list.py
@@ -1,5 +1,24 @@
 my_list = list()
-my_list.insert(10, 20, 30, 40)
+print(type(my_list))
 
+my_list.append(10)
+my_list.append(20)
+my_list.append(30)
+my_list.append(40)
+print(my_list)
 
+my_list.insert(1, 15)
+print(my_list)
+
+my_list.append(50) 
+my_list.append(60)
+my_list.append(70)
+print(my_list)
+
+my_list.pop(-1)
+print(my_list)
+
+my_list.sort()
 print (my_list)
+
+print(my_list.index(30))
